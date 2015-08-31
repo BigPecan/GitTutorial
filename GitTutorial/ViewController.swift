681c692d9c740bd7c1fdf17365e84717fbd00e2a
BigPecan <Jayrmcc@sbcglobal.net>
2015-08-31 12:35:11 -0500

    Looking at how Git Works

--- GitTutorial/ViewController.swift
+++ GitTutorial/ViewController.swift
@@ -13,12 +13,9 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        print("Hey We're Here")
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
 
 
 }
